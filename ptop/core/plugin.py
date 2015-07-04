dd8d0bad1089ab76982878864431add3ededb247
Ankush Sharma <ankprashar@gmail.com>
2015-07-05 02:39:42 +0530

    update : change the local variable to retain the reference

--- ptop/core/plugin.py
+++ ptop/core/plugin.py
@@ -4,11 +4,11 @@
     This module define the BaseClass for plugin. It defines the basic rules that are to be followed by a new plugin.
 '''
 
-class Plugin:
+class Plugin(object):
     ''' 
         Base Plugin class
     '''
-    def __init__(self,name,sensorType,interval,initialValue):
+    def __init__(self,name,sensorType,interval):
         '''creates an instance of the class
 
         Initialize the instance of a Plugin class.
@@ -16,14 +16,13 @@ class Plugin:
         :param name: Name of the plugin
         :param sensorType: How to render the plugin on the screen
         :param interval: The interval after which to update the stats
-        :param initialValue: The initial value to be set for the plugin
         :type sensorType: Chart or Table
         :rtype: Instance of Plugin class
         '''
         self.name = name
         self.type = sensorType
         self.interval = interval
-        self.currentValue = initialValue
+        self.currentValue = {}
 
     def update(self):
         '''updates the plugin currentValue
