ac12249c347a8474f9e67913de50fdba4685d9c8
Ankush Sharma <ankprashar@gmail.com>
2015-07-07 16:23:02 +0530

    add : logger configuration

--- ptop/__init__.py
+++ ptop/__init__.py
@@ -1,4 +1,14 @@
 import sys,os
+import logging
+
+# setting the config
+logging.basicConfig(filename='./logs/ptop.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
+
+logger = logging.getLogger('ptop_logger')
 
 sys.path.append( os.path.join(
     (os.path.dirname(__file__)),
