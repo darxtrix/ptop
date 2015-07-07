fd5a4a6915576cdd93c30f52ed3e1e05ff664f62
Ankush Sharma <ankprashar@gmail.com>
2015-07-07 06:24:22 +0530

    update : passed global stop event

--- ptop/main.py
+++ ptop/main.py
@@ -1,13 +1,26 @@
+import threading
 from ptop.statistics import Statistics
 from interfaces import PtopGUI
 from plugins import SENSORS_LIST
 
 def main():
-    s = Statistics(SENSORS_LIST)
-    # internally uses a thread Job 
-    s.generate()
-    app = PtopGUI(s.statistics)
-    app.run()
+    try:
+        # app wide global stop flag
+        global_stop_event = threading.Event()
+
+        s = Statistics(SENSORS_LIST,global_stop_event)
+        # internally uses a thread Job 
+        s.generate()
+
+        app = PtopGUI(s.statistics,global_stop_event)
+        # blocking call
+        app.run()
+
+    # catch the kill signals here and perform the clean up
+    except KeyboardInterrupt:
+        global_stop_event.set()
+        raise SystemExit
+
 
 
 if __name__ == '__main__':
