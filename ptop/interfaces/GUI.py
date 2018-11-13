6a0e7410d8d0b8834e5ccad10185673806107fb6
Ankush Sharma <ankcodes@gmail.com>
2018-11-13 16:33:55 +0530

    Python3 compatibility fix, PR 44 clean up

--- ptop/interfaces/GUI.py
+++ ptop/interfaces/GUI.py
@@ -374,19 +374,6 @@ class PtopGUI(npyscreen.NPSApp):
             system_info = self.statistics['System']['text']
             cpu_info = self.statistics['CPU']['graph']
 
-            #Convert tottal seconds to hour:minute:second format
-            ttotal = system_info['running_time'].total_seconds()
-            hhours = int((ttotal/60)//60)
-            mmins = int((ttotal-60*60*hhours)//60)
-            ssecs = int(ttotal - hhours*60*60 - mmins*60)
-            mmins = str(mmins)
-            hhours = str(hhours)
-            ssecs = str(ssecs)
-            hhours = '0'*(2-len(hhours)) + hhours
-            mmins = '0'*(2-len(mmins)) + mmins
-            ssecs = '0'*(2-len(ssecs)) + ssecs
-            timeStr = hhours + ':' + mmins + ':' + ssecs
-
             #### Overview information ####
 
             row1 = "Disk Usage (/) {4}{0: <6}/{1: >6} MB{4}{2: >2} %{5}Processes{4}{3: <8}".format(disk_info["used"],
@@ -406,7 +393,7 @@ class PtopGUI(npyscreen.NPSApp):
             row3 = "Main Memory    {4}{0: <6}/{1: >6} MB{4}{2: >2} %{5}Boot Time{4}{3: <8}".format(memory_info["active"],
                                                                                                    memory_info["total"],
                                                                                                    memory_info["percentage"],
-                                                                                                   timeStr,
+                                                                                                   str(system_info["running_time"]),
                                                                                                    " "*int(4*self.X_SCALING_FACTOR),
                                                                                                    " "*int(9*self.X_SCALING_FACTOR))
 
