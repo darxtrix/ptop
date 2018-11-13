3d35890cc081fda066ed222fdedfbda8ab989abf
vinusankars <vinusankars@gmail.com>
2018-11-13 23:10:36 +0530

    Update main.py
    Added dictionary for sensor rates.

--- ptop/main.py
+++ ptop/main.py
@@ -115,8 +115,10 @@ def main():
         psrt = (1000 if psrt>1000 else psrt)
         ssrt = (results.ssrt if results.ssrt else 1000)
         ssrt = (1000 if ssrt>1000 else ssrt)
-
-        sensor_rate = [csrt, dsrt, msrt, psrt, ssrt]
+    
+        srt = [csrt, dsrt, msrt, psrt, ssrt]
+        sensor_rate = {SENSORS_LIST[i]: srt[i] for i in range(len(SENSORS_LIST))}
+        
         # app wide global stop flag
         global_stop_event = threading.Event()
 
